01567c701f36fbfc68de5e130552d9a98453b6f9
Daniil Basmanov <basmanovdaniil@gmail.com>
2014-11-15 23:12:03 +0300

    Added Directions enum

---
+++ Directions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProceduralToolkit
+{
+    [Flags]
+    public enum Directions
+    {
+        None = 0,
+        Left = 1,
+        Right = 2,
+        Down = 4,
+        Up = 8,
+        Back = 16,
+        Forward = 32,
+        XAxis = Left | Right,
+        YAxis = Down | Up,
+        ZAxis = Back | Forward,
+        All = Left | Right | Down | Up | Back | Forward
+    }
+}
