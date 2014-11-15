1c24d1e6d29e583faaefc24f820bcf4f02df2eee
Daniil Basmanov <basmanovdaniil@gmail.com>
2014-11-15 12:01:53 +0300

    Added new extensions for Color

--- ColorE.cs
+++ ColorE.cs
@@ -165,5 +165,25 @@ namespace ProceduralToolkit
             }
             return bytes;
         }
+
+        public static Color WithR(this Color color, float r)
+        {
+            return new Color(r, color.g, color.b, color.a);
+        }
+
+        public static Color WithG(this Color color, float g)
+        {
+            return new Color(color.r, g, color.b, color.a);
+        }
+
+        public static Color WithB(this Color color, float b)
+        {
+            return new Color(color.r, color.g, b, color.a);
+        }
+
+        public static Color WithA(this Color color, float a)
+        {
+            return new Color(color.r, color.g, color.b, a);
+        }
     }
 }
