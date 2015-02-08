538952b2e943f9b821ec55b6337bb13b07faf928
Daniil Basmanov <basmanovdaniil@gmail.com>
2015-02-08 15:29:27 +0300

    Added mesh saver

---
+++ Editor/MeshFilterExtension.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace ProceduralToolkit.Editor
+{
+    public class MeshFilterExtension
+    {
+        private const string menuPath = "CONTEXT/MeshFilter/Save Mesh";
+
+        [MenuItem(menuPath)]
+        private static void SaveMesh(MenuCommand menuCommand)
+        {
+            var meshFilter = (MeshFilter) menuCommand.context;
+            var mesh = meshFilter.sharedMesh;
+
+            var path = EditorUtility.SaveFilePanelInProject("Save Mesh", mesh.name, "asset", "Save Mesh");
+            if (string.IsNullOrEmpty(path))
+            {
+                return;
+            }
+            AssetDatabase.CreateAsset(Object.Instantiate(mesh), path);
+        }
+
+        [MenuItem(menuPath, true)]
+        private static bool SaveMeshTest(MenuCommand menuCommand)
+        {
+            var meshFilter = (MeshFilter) menuCommand.context;
+            return meshFilter.sharedMesh != null;
+        }
+    }
+}
