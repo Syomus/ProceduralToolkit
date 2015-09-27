50b95c62a0b797d3120ea18b98f6917022ef2644
Daniil Basmanov <basmanovdaniil@gmail.com>
2015-09-27 18:28:13 +0300

    Added commentaries to ColorE

--- ColorE.cs
+++ ColorE.cs
@@ -126,21 +126,33 @@ namespace ProceduralToolkit
             return g;
         }
 
+        /// <summary>
+        /// Returns new color with modified red component
+        /// </summary>
         public static Color WithR(this Color color, float r)
         {
             return new Color(r, color.g, color.b, color.a);
         }
 
+        /// <summary>
+        /// Returns new color with modified green component
+        /// </summary>
         public static Color WithG(this Color color, float g)
         {
             return new Color(color.r, g, color.b, color.a);
         }
 
+        /// <summary>
+        /// Returns new color with modified blue component
+        /// </summary>
         public static Color WithB(this Color color, float b)
         {
             return new Color(color.r, color.g, b, color.a);
         }
 
+        /// <summary>
+        /// Returns new color with modified alpha component
+        /// </summary>
         public static Color WithA(this Color color, float a)
         {
             return new Color(color.r, color.g, color.b, a);
