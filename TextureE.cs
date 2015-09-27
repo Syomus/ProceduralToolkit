a57356bce6d4529f34cd73e875c25059ad2520f7
Daniil Basmanov <basmanovdaniil@gmail.com>
2015-09-27 20:34:06 +0300

    Removed texture constructors, added DrawCircle, DrawGradient and DrawGradientRect

--- TextureE.cs
+++ TextureE.cs
@@ -5,35 +5,9 @@ namespace ProceduralToolkit
 {
     public static class TextureE
     {
-        public static Texture2D checker
-        {
-            get { return Checker(Color.white, Color.black); }
-        }
-
-        public static Texture2D Pixel(Color color)
-        {
-            var texture = new Texture2D(1, 1);
-            texture.SetPixel(0, 0, color);
-            texture.Apply();
-            return texture;
-        }
-
-        public static Texture2D Gradient(Color top, Color bottom)
-        {
-            var texture = new Texture2D(2, 2) {wrapMode = TextureWrapMode.Clamp};
-            texture.SetPixels(new[] {bottom, bottom, top, top});
-            texture.Apply();
-            return texture;
-        }
-
-        public static Texture2D Checker(Color first, Color second)
-        {
-            var texture = new Texture2D(2, 2) {filterMode = FilterMode.Point};
-            texture.SetPixels(new[] {second, first, first, second});
-            texture.Apply();
-            return texture;
-        }
-
+        /// <summary>
+        /// Draws line on texture using Bresenham's or Wu's algorithm
+        /// </summary>
         public static void DrawLine(this Texture2D texture, int x0, int y0, int x1, int y1, Color color, bool AA = false)
         {
             if (AA)
@@ -49,24 +23,87 @@ namespace ProceduralToolkit
             }
         }
 
+        /// <summary>
+        /// Draws circle on texture using Bresenham's algorithm
+        /// </summary>
+        public static void DrawCircle(this Texture2D texture, int x, int y, int radius, Color color)
+        {
+            Action<int, int> draw = (_x, _y) => texture.SetPixel(_x, _y, color);
+            PTUtils.BresenhamCircle(x, y, radius, draw);
+        }
+
+        /// <summary>
+        /// Draws filled rectangle on texture
+        /// </summary>
         public static void DrawRect(this Texture2D texture, int x, int y, int blockWidth, int blockHeight, Color color)
         {
             var colors = new Color[blockWidth*blockHeight];
-            for (int i = 0; i < blockHeight; i++)
+            for (int _y = 0; _y < blockHeight; _y++)
             {
-                for (int j = 0; j < blockWidth; j++)
+                for (int _x = 0; _x < blockWidth; _x++)
                 {
-                    colors[j + i*blockWidth] = color;
+                    colors[_x + _y*blockWidth] = color;
                 }
             }
             texture.SetPixels(x, y, blockWidth, blockHeight, colors);
         }
 
+        /// <summary>
+        /// Fills texture with gradient
+        /// </summary>
+        public static void DrawGradient(this Texture2D texture, Gradient gradient, Directions progressionDirection)
+        {
+            texture.DrawGradientRect(0, 0, texture.width, texture.height, gradient, progressionDirection);
+        }
+
+        /// <summary>
+        /// Draws gradient rectangle on texture
+        /// </summary>t
+        public static void DrawGradientRect(this Texture2D texture, int x, int y, int blockWidth, int blockHeight,
+            Gradient gradient, Directions progressionDirection)
+        {
+            Func<int, int, Color> getColor;
+            switch (progressionDirection)
+            {
+                case Directions.Left:
+                    getColor = (_x, _y) => gradient.Evaluate(1 - (float) _x/(float) blockWidth);
+                    break;
+                case Directions.Right:
+                    getColor = (_x, _y) => gradient.Evaluate((float) _x/(float) blockWidth);
+                    break;
+                case Directions.Down:
+                    getColor = (_x, _y) => gradient.Evaluate(1 - (float) _y/(float) blockHeight);
+                    break;
+                case Directions.Up:
+                    getColor = (_x, _y) => gradient.Evaluate((float) _y/(float) blockHeight);
+                    break;
+                default:
+                    Debug.LogError("Not supported direction: " + progressionDirection);
+                    return;
+            }
+
+            var colors = new Color[blockWidth*blockHeight];
+            for (int _y = 0; _y < blockHeight; _y++)
+            {
+                for (int _x = 0; _x < blockWidth; _x++)
+                {
+                    colors[_x + _y*blockWidth] = getColor(_x, _y);
+                }
+            }
+            texture.SetPixels(x, y, blockWidth, blockHeight, colors);
+        }
+
+        /// <summary>
+        /// Fills texture with white color
+        /// </summary>
         public static void Clear(this Texture2D texture)
         {
             Clear(texture, Color.white);
         }
 
+        /// <summary>
+        /// Fills texture with specified color
+        /// </summary>
         public static void Clear(this Texture2D texture, Color color)
         {
             var pixels = texture.GetPixels();
