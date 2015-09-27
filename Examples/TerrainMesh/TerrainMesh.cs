710d1dc07ee7760c1b7f7a90c091873feb4b0082
Daniil Basmanov <basmanovdaniil@gmail.com>
2015-09-27 23:44:03 +0300

    Refactored TerrainMesh

--- Examples/TerrainMesh/TerrainMesh.cs
+++ Examples/TerrainMesh/TerrainMesh.cs
@@ -1,54 +1,92 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 namespace ProceduralToolkit.Examples
 {
     /// <summary>
     /// A simple terrain based on Perlin noise and coloured according to height
     /// </summary>
-    [RequireComponent(typeof (MeshRenderer), typeof (MeshFilter))]
     public class TerrainMesh : MonoBehaviour
     {
-        public float xSize = 10;
-        public float zSize = 10;
-        public int xSegments = 100;
-        public int zSegments = 100;
-        public float scale = 100;
+        public MeshFilter meshFilter;
 
-        private MeshDraft draft;
-        private Gradient gradient;
+        public Slider xSizeSlider;
+        public Text xSizeText;
 
-        private void Start()
+        public Slider zSizeSlider;
+        public Text zSizeText;
+
+        public Slider xSegmentsSlider;
+        public Text xSegmentsText;
+
+        public Slider zSegmentsSlider;
+        public Text zSegmentsText;
+
+        public Slider noiseScaleSlider;
+        public Text noiseScaleText;
+
+        public Button generateButton;
+
+        private int xSize = 10;
+        private int zSize = 10;
+        private int xSegments = 100;
+        private int zSegments = 100;
+        private int noiseScale = 10;
+
+        private void Awake()
         {
-            draft = MeshDraft.Plane(xSize, zSize, xSegments, zSegments);
-            draft.Move(Vector3.left*xSize/2 + Vector3.back*zSize/2);
+            SetupSlider(xSizeSlider, xSizeText, xSize, value => xSize = value);
+            SetupSlider(zSizeSlider, zSizeText, zSize, value => zSize = value);
+            SetupSlider(xSegmentsSlider, xSegmentsText, xSegments, value => xSegments = value);
+            SetupSlider(zSegmentsSlider, zSegmentsText, zSegments, value => zSegments = value);
+            SetupSlider(noiseScaleSlider, noiseScaleText, noiseScale, value => noiseScale = value);
+
+            generateButton.onClick.AddListener(Generate);
+
             Generate();
         }
 
-        private void Update()
+        private void SetupSlider(Slider slider, Text text, float defaultValue, Action<int> onValueChanged)
         {
-            if (Input.GetMouseButtonDown(0))
+            slider.value = defaultValue;
+            slider.onValueChanged.AddListener(value =>
             {
-                Generate();
-            }
+                int intValue = Mathf.FloorToInt(value);
+                onValueChanged(intValue);
+                text.text = intValue.ToString();
+            });
+            text.text = defaultValue.ToString();
         }
 
         private void Generate()
         {
-            gradient = RandomE.gradientHSV;
+            var mesh = TerrainDraft().ToMesh();
+            meshFilter.mesh = mesh;
+        }
+
+        private MeshDraft TerrainDraft()
+        {
+            var draft = MeshDraft.Plane(xSize, zSize, xSegments, zSegments);
+            draft.Move(Vector3.left*xSize/2 + Vector3.back*zSize/2);
+
+            var gradient = RandomE.gradientHSV;
 
             var noiseOffset = new Vector2(Random.Range(0f, 100f), Random.Range(0f, 100f));
             draft.colors.Clear();
             for (int i = 0; i < draft.vertices.Count; i++)
             {
-                var vertex = draft.vertices[i];
-                var x = scale*vertex.x/xSegments + noiseOffset.x;
-                var y = scale*vertex.z/zSegments + noiseOffset.y;
-                var noise = Mathf.PerlinNoise(x, y);
+                // Calculate noise value
+                Vector3 vertex = draft.vertices[i];
+                float x = noiseScale*vertex.x/xSize + noiseOffset.x;
+                float y = noiseScale*vertex.z/zSize + noiseOffset.y;
+                float noise = Mathf.PerlinNoise(x, y);
                 draft.vertices[i] = new Vector3(vertex.x, noise, vertex.z);
+
                 draft.colors.Add(gradient.Evaluate(noise));
             }
-
-            GetComponent<MeshFilter>().mesh = draft.ToMesh();
+            return draft;
         }
     }
 }
