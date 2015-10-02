2ae242d1c6b0710b44862740775b9ab3a7dd8611
Daniil Basmanov <basmanovdaniil@gmail.com>
2015-10-02 10:38:37 +0300

    Added TextControl

---
+++ Examples/UI/TextControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ProceduralToolkit.Examples.UI
+{
+    public class TextControl : MonoBehaviour
+    {
+        public Text headerText;
+
+        public void Initialize(string header)
+        {
+            name = header;
+            headerText.text = header;
+        }
+    }
+}
