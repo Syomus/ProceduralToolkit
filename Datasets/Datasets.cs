fb177843b3e1fbd20ce6924c2c96ec6afd450631
Daniil Basmanov <basmanovdaniil@gmail.com>
2015-11-09 20:48:49 +0300

    Replaced static readonly with const in Datasets

--- Datasets/Datasets.cs
+++ Datasets/Datasets.cs
@@ -8,26 +8,26 @@
         /// <summary>
         /// Lowercase letters from a to z
         /// </summary>
-        public static readonly string lowercase = "abcdefghijklmnopqrstuvwxyz";
+        public const string lowercase = "abcdefghijklmnopqrstuvwxyz";
 
         /// <summary>
         /// Uppercase letters from A to Z
         /// </summary>
-        public static readonly string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public const string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
         /// <summary>
         /// Digits from zero to nine
         /// </summary>
-        public static readonly string digits = "0123456789";
+        public const string digits = "0123456789";
 
         /// <summary>
         /// The concatenation of the strings <see cref="lowercase"/> and <see cref="uppercase"/>
         /// </summary>
-        public static readonly string letters = lowercase + uppercase;
+        public const string letters = lowercase + uppercase;
 
         /// <summary>
         /// The concatenation of the strings <see cref="letters"/> and <see cref="digits"/>
         /// </summary>
-        public static readonly string alphanumerics = letters + digits;
+        public const string alphanumerics = letters + digits;
     }
 }
