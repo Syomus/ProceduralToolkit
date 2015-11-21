ee393e7619f1123347de27a60d9623ea44312fc5
Daniil Basmanov <basmanovdaniil@gmail.com>
2015-11-21 20:06:49 +0300

    Fixed build warning

--- Examples/CameraRotator.cs
+++ Examples/CameraRotator.cs
@@ -10,7 +10,7 @@ namespace ProceduralToolkit.Examples
     [RequireComponent(typeof (Image))]
     public class CameraRotator : UIBehaviour, IDragHandler
     {
-        public new Transform camera;
+        public Transform cameraTransform;
         public Transform target;
         [Header("Position")]
         public float distanceMin = 10;
@@ -37,19 +37,20 @@ namespace ProceduralToolkit.Examples
             tiltAngle = (tiltMin + tiltMax)/2;
             distance = scrollDistance = (distanceMax + distanceMin)/2;
 
-            if (camera == null || target == null) return;
+            if (cameraTransform == null || target == null) return;
 
-            camera.rotation = rotation = Quaternion.Euler(tiltAngle, lookAngle, 0);
-            camera.position = CalculateCameraPosition();
+            cameraTransform.rotation = rotation = Quaternion.Euler(tiltAngle, lookAngle, 0);
+            cameraTransform.position = CalculateCameraPosition();
         }
 
         private void LateUpdate()
         {
-            if (camera == null || target == null) return;
+            if (cameraTransform == null || target == null) return;
 
-            if (camera.rotation != rotation)
+            if (cameraTransform.rotation != rotation)
             {
-                camera.rotation = Quaternion.Lerp(camera.rotation, rotation, Time.deltaTime*rotationSpeed);
+                cameraTransform.rotation = Quaternion.Lerp(cameraTransform.rotation, rotation,
+                    Time.deltaTime*rotationSpeed);
             }
 
             float scroll = Input.GetAxis("Mouse ScrollWheel");
@@ -64,12 +65,12 @@ namespace ProceduralToolkit.Examples
                 distance = Mathf.SmoothDamp(distance, scrollDistance, ref velocity, Time.deltaTime*scrollSmoothing);
             }
 
-            camera.position = CalculateCameraPosition();
+            cameraTransform.position = CalculateCameraPosition();
         }
 
         public void OnDrag(PointerEventData eventData)
         {
-            if (camera == null || target == null) return;
+            if (cameraTransform == null || target == null) return;
 
             lookAngle += eventData.delta.x*rotationSensitivity;
             tiltAngle -= eventData.delta.y*rotationSensitivity;
@@ -79,7 +80,7 @@ namespace ProceduralToolkit.Examples
 
         private Vector3 CalculateCameraPosition()
         {
-            return target.position + camera.rotation*(Vector3.back*distance) + Vector3.up*yOffset;
+            return target.position + cameraTransform.rotation*(Vector3.back*distance) + Vector3.up*yOffset;
         }
     }
 }
