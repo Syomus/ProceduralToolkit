4463d04adb62b3eaf45f94515d24e256b10f488f
Daniil Basmanov <basmanovdaniil@gmail.com>
2016-08-27 19:37:26 +0300

    Added ChairGeneratorUIEditor

--- Examples/ChairGenerator/ChairGeneratorUI.cs
+++ Examples/ChairGenerator/ChairGeneratorUI.cs
@@ -7,51 +7,77 @@ namespace ProceduralToolkit.Examples.UI
         public MeshFilter meshFilter;
         public RectTransform leftPanel;
 
-        private float legWidth = 0.07f;
-        private float legHeight = 0.7f;
-        private float seatWidth = 0.7f;
-        private float seatDepth = 0.7f;
-        private float seatHeight = 0.05f;
-        private float backHeight = 0.8f;
-        private bool hasStretchers = true;
-        private bool hasArmrests = false;
+        [Space]
+        [Range(minLegWidth, maxLegWidth)]
+        public float legWidth = 0.07f;
+        [Range(minLegHeight, maxLegHeight)]
+        public float legHeight = 0.7f;
+        [Range(minSeatWidth, maxSeatWidth)]
+        public float seatWidth = 0.7f;
+        [Range(minSeatDepth, maxSeatDepth)]
+        public float seatDepth = 0.7f;
+        [Range(minSeatHeight, maxSeatHeight)]
+        public float seatHeight = 0.05f;
+        [Range(minBackHeight, maxBackHeight)]
+        public float backHeight = 0.8f;
+        public bool hasStretchers = true;
+        public bool hasArmrests = false;
+
+        private const float minLegWidth = 0.05f;
+        private const float maxLegWidth = 0.12f;
+        private const float minLegHeight = 0.5f;
+        private const float maxLegHeight = 1.2f;
+        private const float minSeatWidth = 0.5f;
+        private const float maxSeatWidth = 1.2f;
+        private const float minSeatDepth = 0.3f;
+        private const float maxSeatDepth = 1.2f;
+        private const float minSeatHeight = 0.03f;
+        private const float maxSeatHeight = 0.2f;
+        private const float minBackHeight = 0.5f;
+        private const float maxBackHeight = 1.3f;
 
         private void Awake()
         {
             Generate();
 
-            InstantiateControl<SliderControl>(leftPanel).Initialize("Leg width", 0.05f, 0.12f, legWidth, value =>
-            {
-                legWidth = value;
-                Generate();
-            });
-            InstantiateControl<SliderControl>(leftPanel).Initialize("Leg height", 0.5f, 1.2f, legHeight, value =>
-            {
-                legHeight = value;
-                Generate();
-            });
+            InstantiateControl<SliderControl>(leftPanel)
+                .Initialize("Leg width", minLegWidth, maxLegWidth, legWidth, value =>
+                {
+                    legWidth = value;
+                    Generate();
+                });
+            InstantiateControl<SliderControl>(leftPanel)
+                .Initialize("Leg height", minLegHeight, maxLegHeight, legHeight, value =>
+                {
+                    legHeight = value;
+                    Generate();
+                });
 
-            InstantiateControl<SliderControl>(leftPanel).Initialize("Seat width", 0.5f, 1.2f, seatWidth, value =>
-            {
-                seatWidth = value;
-                Generate();
-            });
-            InstantiateControl<SliderControl>(leftPanel).Initialize("Seat depth", 0.3f, 1.2f, seatDepth, value =>
-            {
-                seatDepth = value;
-                Generate();
-            });
-            InstantiateControl<SliderControl>(leftPanel).Initialize("Seat height", 0.03f, 0.2f, seatHeight, value =>
-            {
-                seatHeight = value;
-                Generate();
-            });
+            InstantiateControl<SliderControl>(leftPanel)
+                .Initialize("Seat width", minSeatWidth, maxSeatWidth, seatWidth, value =>
+                {
+                    seatWidth = value;
+                    Generate();
+                });
+            InstantiateControl<SliderControl>(leftPanel)
+                .Initialize("Seat depth", minSeatDepth, maxSeatDepth, seatDepth, value =>
+                {
+                    seatDepth = value;
+                    Generate();
+                });
+            InstantiateControl<SliderControl>(leftPanel)
+                .Initialize("Seat height", minSeatHeight, maxSeatHeight, seatHeight, value =>
+                {
+                    seatHeight = value;
+                    Generate();
+                });
 
-            InstantiateControl<SliderControl>(leftPanel).Initialize("Back height", 0.5f, 1.3f, backHeight, value =>
-            {
-                backHeight = value;
-                Generate();
-            });
+            InstantiateControl<SliderControl>(leftPanel)
+                .Initialize("Back height", minBackHeight, maxBackHeight, backHeight, value =>
+                {
+                    backHeight = value;
+                    Generate();
+                });
 
             InstantiateControl<ToggleControl>(leftPanel).Initialize("Has stretchers", hasStretchers, value =>
             {
@@ -67,7 +93,7 @@ namespace ProceduralToolkit.Examples.UI
             InstantiateControl<ButtonControl>(leftPanel).Initialize("Generate", Generate);
         }
 
-        private void Generate()
+        public void Generate()
         {
             var draft = ChairGenerator.Chair(legWidth, legHeight, seatWidth, seatDepth, seatHeight, backHeight,
                 hasStretchers, hasArmrests);
---
+++ Examples/ChairGenerator/Editor/ChairGeneratorUIEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ProceduralToolkit.Examples.UI
+{
+    [CustomEditor(typeof (ChairGeneratorUI))]
+    public class ChairGeneratorUIEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            EditorGUILayout.Space();
+            if (GUILayout.Button("Generate"))
+            {
+                ((ChairGeneratorUI) target).Generate();
+            }
+        }
+    }
+}
