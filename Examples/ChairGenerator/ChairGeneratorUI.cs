4f904dda67ac35389e55ec78e38e25d9c26aeb77
Daniil Basmanov <basmanovdaniil@gmail.com>
2016-09-05 23:32:59 +0300

    Added platform to ChairGenerator, changed palette

--- Examples/ChairGenerator/ChairGeneratorUI.cs
+++ Examples/ChairGenerator/ChairGeneratorUI.cs
@@ -5,7 +5,8 @@ namespace ProceduralToolkit.Examples.UI
 {
     public class ChairGeneratorUI : UIBase
     {
-        public MeshFilter meshFilter;
+        public MeshFilter chairMeshFilter;
+        public MeshFilter platformMeshFilter;
         public RectTransform leftPanel;
 
         [Space]
@@ -36,6 +37,11 @@ namespace ProceduralToolkit.Examples.UI
         private const float maxSeatHeight = 0.2f;
         private const float minBackHeight = 0.5f;
         private const float maxBackHeight = 1.3f;
+
+        private const float platformBaseOffset = 0.05f;
+        private const float platformHeight = 0.05f;
+        private const float platformRadiusOffset = 0.5f;
+        private const int platformSegments = 128;
 
         private List<ColorHSV> targetPalette = new List<ColorHSV>();
         private List<ColorHSV> currentPalette = new List<ColorHSV>();
@@ -102,22 +108,57 @@ namespace ProceduralToolkit.Examples.UI
 
         private void Update()
         {
-            SkyBoxGenerator.LerpSkybox(RenderSettings.skybox, currentPalette, targetPalette, 1, 2, 3, Time.deltaTime);
+            SkyBoxGenerator.LerpSkybox(RenderSettings.skybox, currentPalette, targetPalette, 2, 3, 4, Time.deltaTime);
         }
 
         public void Generate()
         {
-            targetPalette = RandomE.TriadicPalette(0.5f, 0.75f);
-            targetPalette.Add(ColorHSV.Lerp(targetPalette[1], targetPalette[2], 0.5f));
+            targetPalette = RandomE.TetradicPalette(0.25f, 0.75f);
+            targetPalette.Add(ColorHSV.Lerp(targetPalette[2], targetPalette[3], 0.5f));
 
-            var draft = ChairGenerator.Chair(legWidth, legHeight, seatWidth, seatDepth, seatHeight, backHeight,
+            var chairDraft = ChairGenerator.Chair(legWidth, legHeight, seatWidth, seatDepth, seatHeight, backHeight,
                 hasStretchers, hasArmrests, targetPalette[0].WithS(0.8f).WithV(0.8f).ToColor());
+            var chairMesh = chairDraft.ToMesh();
+            chairMesh.RecalculateBounds();
+            chairMeshFilter.mesh = chairMesh;
 
-            var circle = MeshDraft.TriangleFan(PTUtils.PointsOnCircle3XZ(2, 128));
-            circle.Paint(new Color(0.8f, 0.8f, 0.8f, 1));
-            draft.Add(circle);
+            float chairRadius = Mathf.Sqrt(seatWidth/2f*seatWidth/2f + seatDepth/2f*seatDepth/2f);
+            float platformRadius = chairRadius + platformRadiusOffset;
 
-            meshFilter.mesh = draft.ToMesh();
+            var platformMesh = Platform(platformRadius, platformBaseOffset, platformSegments, platformHeight).ToMesh();
+            platformMesh.RecalculateBounds();
+            platformMeshFilter.mesh = platformMesh;
+        }
+
+        private static MeshDraft Platform(float radius, float baseOffset, int segments, float heignt)
+        {
+            float segmentAngle = 360f/segments;
+            float currentAngle = 0;
+
+            var lowerRing = new List<Vector3>(segments);
+            var upperRing = new List<Vector3>(segments);
+            for (var i = 0; i < segments; i++)
+            {
+                var lowerPoint = PTUtils.PointOnCircle3XZ(radius + baseOffset, currentAngle);
+                lowerRing.Add(lowerPoint + Vector3.down*heignt);
+
+                var upperPoint = PTUtils.PointOnCircle3XZ(radius, currentAngle);
+                upperRing.Add(upperPoint);
+                currentAngle -= segmentAngle;
+            }
+
+            var platform = new MeshDraft {name = "Platform"};
+            var bottom = MeshDraft.TriangleFan(lowerRing);
+            bottom.Add(MeshDraft.Band(lowerRing, upperRing));
+            bottom.Paint(new Color(0.5f, 0.5f, 0.5f, 1));
+            platform.Add(bottom);
+
+            upperRing.Reverse();
+            var top = MeshDraft.TriangleFan(upperRing);
+            top.Paint(new Color(0.8f, 0.8f, 0.8f, 1));
+            platform.Add(top);
+
+            return platform;
         }
     }
 }
