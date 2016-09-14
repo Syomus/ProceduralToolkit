0f1d95492263bcff69c0dc4aba22a1fd62f604e1
Daniil Basmanov <basmanovdaniil@gmail.com>
2016-09-14 21:46:38 +0300

    Added VectorE.ToVector3XY and ToVector3YZ

--- VectorE.cs
+++ VectorE.cs
@@ -14,9 +14,19 @@ namespace ProceduralToolkit
             return new Vector2(0, vector.y);
         }
 
+        public static Vector3 ToVector3XY(this Vector2 vector)
+        {
+            return new Vector3(vector.x, vector.y, 0);
+        }
+
         public static Vector3 ToVector3XZ(this Vector2 vector)
         {
             return new Vector3(vector.x, 0, vector.y);
+        }
+
+        public static Vector3 ToVector3YZ(this Vector2 vector)
+        {
+            return new Vector3(0, vector.x, vector.y);
         }
 
         public static Vector3 OnlyX(this Vector3 vector)
