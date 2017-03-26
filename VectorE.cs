e6c868ed6b3f5c33bdc81503f65051e07f03dc24
Daniil Basmanov <basmanovdaniil@gmail.com>
2017-03-26 19:58:41 +0300

    Added VectorE.DistanceToLine and VectorE.ProjectOnLine

--- VectorE.cs
+++ VectorE.cs
@@ -72,6 +72,46 @@ namespace ProceduralToolkit
                 -vector.x*sin + vector.y*cos);
         }
 
+        /// <summary>
+        /// Returns the distance between <paramref name="point"/> and a line defined by <paramref name="lineA"/> and <paramref name="lineB"/>
+        /// </summary>
+        public static float DistanceToLine(this Vector2 point, Vector2 lineA, Vector2 lineB)
+        {
+            return Vector2.Distance(point, ProjectOnLine(point, lineA, lineB));
+        }
+
+        /// <summary>
+        /// Projects a <paramref name="point"/> onto a line defined by <paramref name="lineA"/> and <paramref name="lineB"/>
+        /// </summary>
+        public static Vector2 ProjectOnLine(this Vector2 point, Vector2 lineA, Vector2 lineB)
+        {
+            float projectedX;
+            return ProjectOnLine(point, lineA, lineB, out projectedX);
+        }
+
+        /// <summary>
+        /// Projects a <paramref name="point"/> onto a line defined by <paramref name="lineA"/> and <paramref name="lineB"/>
+        /// </summary>
+        /// <param name="projectedX">Normalized position of projected point on a line segment. 
+        /// Value of zero means that point coincides with <paramref name="lineA"/>. 
+        /// Value of one means that point coincides with <paramref name="lineB"/>.</param>
+        public static Vector2 ProjectOnLine(this Vector2 point, Vector2 lineA, Vector2 lineB, out float projectedX)
+        {
+            Vector2 direction = lineB - lineA;
+            Vector2 toPoint = point - lineA;
+
+            float dot = Vector2.Dot(direction, direction);
+            if ((double) dot < (double) Mathf.Epsilon)
+            {
+                Debug.LogError("Invalid line definition. lineA: " + lineA + " lineB: " + lineB);
+                projectedX = 0;
+                return lineA;
+            }
+
+            projectedX = Vector2.Dot(toPoint, direction)/dot;
+            return lineA + direction*projectedX;
+        }
+
         #endregion Vector2
 
         #region Vector3
@@ -122,6 +162,46 @@ namespace ProceduralToolkit
         public static Vector3 OnlyYZ(this Vector3 vector)
         {
             return new Vector3(0, vector.y, vector.z);
+        }
+
+        /// <summary>
+        /// Returns the distance between <paramref name="point"/> and a line defined by <paramref name="lineA"/> and <paramref name="lineB"/>
+        /// </summary>
+        public static float DistanceToLine(this Vector3 point, Vector3 lineA, Vector3 lineB)
+        {
+            return Vector3.Distance(point, ProjectOnLine(point, lineA, lineB));
+        }
+
+        /// <summary>
+        /// Projects a <paramref name="point"/> onto a line defined by <paramref name="lineA"/> and <paramref name="lineB"/>
+        /// </summary>
+        public static Vector3 ProjectOnLine(this Vector3 point, Vector3 lineA, Vector3 lineB)
+        {
+            float projectedX;
+            return ProjectOnLine(point, lineA, lineB, out projectedX);
+        }
+
+        /// <summary>
+        /// Projects a <paramref name="point"/> onto a line defined by <paramref name="lineA"/> and <paramref name="lineB"/>
+        /// </summary>
+        /// <param name="projectedX">Normalized position of projected point on a line segment. 
+        /// Value of zero means that point coincides with <paramref name="lineA"/>. 
+        /// Value of one means that point coincides with <paramref name="lineB"/>.</param>
+        public static Vector3 ProjectOnLine(this Vector3 point, Vector3 lineA, Vector3 lineB, out float projectedX)
+        {
+            Vector3 direction = lineB - lineA;
+            Vector3 toPoint = point - lineA;
+
+            float dot = Vector3.Dot(direction, direction);
+            if ((double) dot < (double) Mathf.Epsilon)
+            {
+                Debug.LogError("Invalid line definition. lineA: " + lineA + " lineB: " + lineB);
+                projectedX = 0;
+                return lineA;
+            }
+
+            projectedX = Vector3.Dot(toPoint, direction)/dot;
+            return lineA + direction*projectedX;
         }
 
         #endregion Vector3
