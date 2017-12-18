170421782e9131257ed0a10bf5ffe440993f9f6d
Daniil Basmanov <basmanovdaniil@gmail.com>
2017-12-18 19:21:32 +0300

    Upgraded to Unity 2017.2.0f3

--- Examples/Mazes/Maze.cs
+++ Examples/Mazes/Maze.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace ProceduralToolkit.Examples
 {
--- Scripts/ArrayE.cs
+++ Scripts/ArrayE.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace ProceduralToolkit
 {
