d1c3049eaba697429120f4a95351be3715ba02cf
Daniil Basmanov <basmanovdaniil@gmail.com>
2017-12-21 14:07:23 +0300

    Added drawing example

---
+++ Examples/Drawing/Drawer.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+namespace ProceduralToolkit.Examples
+{
+    [ExecuteInEditMode]
+    public class Drawer : MonoBehaviour
+    {
+        private const float radius = 1.5f;
+        private const float coneAngle = 30;
+        private const float coneLength = 1;
+
+        private void Update()
+        {
+            Color color = ColorE.aqua;
+            Quaternion rotation = transform.rotation*Quaternion.Euler(0, 0, -120);
+            Vector3 position = transform.position + rotation*Vector3.up;
+
+            DebugE.DrawWireHemisphere(position, rotation, radius, color);
+            DebugE.DrawWireCone(position, rotation, radius, coneAngle, coneLength, color);
+        }
+
+        private void OnRenderObject()
+        {
+            GLE.wireMaterial.SetPass(0);
+            GL.Begin(GL.LINES);
+
+            GL.Color(ColorE.fuchsia);
+            Quaternion rotation = transform.rotation;
+            Vector3 position = transform.position + Vector3.up;
+
+            GLE.DrawWireHemisphere(position, rotation, radius);
+            GLE.DrawWireCone(position, rotation, radius, coneAngle, coneLength);
+
+            GL.End();
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = ColorE.lime;
+            Quaternion rotation = transform.rotation*Quaternion.Euler(0, 0, 120);
+            Vector3 position = transform.position + rotation*Vector3.up;
+
+            GizmosE.DrawWireHemisphere(position, rotation, radius);
+            GizmosE.DrawWireCone(position, rotation, radius, coneAngle, coneLength);
+        }
+    }
+}
