7b29f4e55887edfe01348811f91b9d0495107b56
Steven Vergenz <Steven.Vergenz@microsoft.com>
2018-09-26 15:26:50 -0700

    First draft capsule

---
+++ Examples/Primitives/Capsule.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace ProceduralToolkit.Examples.Primitives
+{
+	[RequireComponent(typeof(MeshRenderer), typeof(MeshFilter))]
+	public class Capsule : MonoBehaviour
+	{
+		public float height = 1f;
+		public float radius = 1f;
+		public int segments = 12;
+		public int rings = 8;
+
+		private void Start()
+		{
+			GetComponent<MeshFilter>().mesh = MeshDraft.Capsule(height, radius, segments, rings).ToMesh();
+		}
+	}
+}
