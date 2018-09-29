e67eef1692f30a112c98aab6c7d2e95145cfaee1
Daniil Basmanov <basmanovdaniil@gmail.com>
2018-09-29 16:11:05 +0300

    MeshDraft.Capsule: Fixed normals, changed parameters, renamed some of the variables

--- Examples/Primitives/Capsule.cs
+++ Examples/Primitives/Capsule.cs
@@ -2,17 +2,17 @@
 
 namespace ProceduralToolkit.Examples.Primitives
 {
-	[RequireComponent(typeof(MeshRenderer), typeof(MeshFilter))]
-	public class Capsule : MonoBehaviour
-	{
-		public float height = 1f;
-		public float radius = 1f;
-		public int segments = 12;
-		public int rings = 8;
+    [RequireComponent(typeof(MeshRenderer), typeof(MeshFilter))]
+    public class Capsule : MonoBehaviour
+    {
+        public float height = 2;
+        public float radius = 0.5f;
+        public int segments = 32;
+        public int rings = 8;
 
-		private void Start()
-		{
-			GetComponent<MeshFilter>().mesh = MeshDraft.Capsule(height, radius, segments, rings).ToMesh();
-		}
-	}
+        private void Start()
+        {
+            GetComponent<MeshFilter>().mesh = MeshDraft.Capsule(height, radius, segments, rings).ToMesh();
+        }
+    }
 }
