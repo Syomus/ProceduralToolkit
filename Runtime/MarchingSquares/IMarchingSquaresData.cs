02f676cb562ae633184704b504f823727d0e1571
Daniil Basmanov <basmanovdaniil@gmail.com>
2021-01-09 15:41:59 +0300

    Made IMarchingSquaresData inherit from IDisposable

--- Runtime/MarchingSquares/IMarchingSquaresData.cs
+++ Runtime/MarchingSquares/IMarchingSquaresData.cs
@@ -1,11 +1,12 @@
+using System;
+
 namespace ProceduralToolkit.MarchingSquares
 {
-    public interface IMarchingSquaresData
+    public interface IMarchingSquaresData : IDisposable
     {
         int dataLengthX { get; }
         int dataLengthY { get; }
         bool useInterpolation { get; }
-        void Dispose();
         bool TestValue(int x, int y);
         bool TestAverage(int x, int y);
         float InverseLerp(int xa, int ya, int xb, int yb);
